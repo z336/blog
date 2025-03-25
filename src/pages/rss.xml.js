37a68bc9c6b00cbacb667f2cefcb316b579e47b1
Jon Coleman <jcoleman1388@gmail.com>
2025-03-25 11:29:28 -0400

    editing rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,10 +2,10 @@ import rss, { pagesGlobToRssItems } from "@astrojs/rss";
 
 export async function GET(context) {
   return rss({
-    title: "Astro Learner | Blog",
-    description: "My journey learning Astro",
+    title: "Jon Coleman",
+    description: "Jon's digital garden.",
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob("./**/*.md")),
+    items: await pagesGlobToRssItems(import.meta.glob("./writing/*.md")),
     customData: `<language>en-us</language>`,
   });
 }
