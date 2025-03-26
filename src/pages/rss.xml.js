2f645b9c10ea285e7442e09cf412c3ff5807676c
Jon Coleman <jcoleman1388@gmail.com>
2025-03-26 12:14:47 -0400

    footer updates wip

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,20 @@
 import rss, { pagesGlobToRssItems } from "@astrojs/rss";
+import sanitizeHtml from "sanitize-html";
 
 export async function GET(context) {
+  const postImportResult = import.meta.glob("./writing/*.md", { eager: true });
+  const posts = Object.values(postImportResult);
+
   return rss({
     title: "Jon Coleman",
-    description: "Jon's digital garden.",
+    description: "My digital garden",
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob("./writing/*.md")),
-    customData: `<language>en-us</language>`,
+    items: await Promise.all(
+      posts.map(async (post) => ({
+        link: post.url,
+        content: sanitizeHtml(await post.compiledContent()),
+        ...post.frontmatter,
+      })),
+    ),
   });
 }
--- src/pages/writing/my-first-blog-post.md
+++ src/pages/writing/my-first-blog-post.md
@@ -37,6 +37,10 @@ Kale quinoa tofu bowl avocado toast chickpea curry seitan beyond burger lentil s
 - Salad
 - Motherfucker
 
+1. Kale
+2. Salad
+3. Motherfucker
+
 Kale quinoa tofu bowl avocado toast chickpea curry seitan beyond burger lentil soup. Spinach smoothie almond milk chia seeds falafel hummus tempeh. Cashew cheese black bean tacos, vegan pancakes, jackfruit pulled pork, and oat milk lattes, Roasted cauliflower, hemp seeds, spirulina, vegan sushi, quinoa stuffed peppers, eggplant parmigiana, beet burgers, cashew dressing, Sourdough bread, roasted vegetables, coconut yogurt parfait, plant-based protein, vegan cheese pizza, mushroom stroganoff, turmeric golden milk, tofu scramble.
 
 ### Another heading
