a4683e027c12c463774468e911e6ccd064e08d0b
Jon Coleman <jcoleman1388@gmail.com>
2025-04-05 18:12:35 -0400

    added a new note

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-// TODO: update with actual site once deployed
 export default defineConfig({
   site: "https://joncoleman.me/",
   markdown: {
