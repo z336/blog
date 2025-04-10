3c78a3ed780578bdfa7e62f214cccada3cb5c309
Jon Coleman <jcoleman1388@gmail.com>
2025-04-10 08:30:23 -0400

    configuring CSS during build

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,8 +9,22 @@ export default defineConfig({
       theme: "none",
     },
   },
-  build: {
-    assetsInlineLimit: 1024, // Link styles externally above 1024 bytes
-    inlineStylesheets: "always", // Build the stylesheet inline in the header
+  vite: {
+    build: {
+      rollupOptions: {
+        output: {
+          assetFileNames: (assetInfo) => {
+            if (assetInfo.name && assetInfo.name.endsWith(".css")) {
+              return "css/styles-[hash][extname]";
+            }
+            return "_astro/[name]-[hash][extname]";
+          },
+        },
+      },
+    },
   },
+  // build: {
+  //   assetsInlineLimit: 1024, // Link styles externally above 1024 bytes
+  //   inlineStylesheets: "always", // Build the stylesheet inline in the header
+  // },
 });
