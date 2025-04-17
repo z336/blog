8017188eba4dcde175d0d04ea9a91f9b84ca270c
Jon Coleman <jcoleman1388@gmail.com>
2025-04-16 21:19:52 -0400

    working on rss feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,17 +1,28 @@
-import rss, { pagesGlobToRssItems } from "@astrojs/rss";
+import rss from "@astrojs/rss";
+import sanitizeHtml from "sanitize-html";
+
+const postImports = import.meta.glob("./**/*.md", { eager: true });
 
 export async function GET(context) {
-  const items = await pagesGlobToRssItems(import.meta.glob("./**/*.md"));
+  const posts = await Promise.all(
+    Object.values(postImports).map(async (post) => {
+      const html = await post.compiledContent();
+      return {
+        title: post.frontmatter.title,
+        pubDate: new Date(post.frontmatter.pubDate),
+        link: post.url,
+        description: sanitizeHtml(html),
+      };
+    }),
+  );
 
-  items.sort((a, b) => {
-    return new Date(b.pubDate).getTime() - new Date(a.pubDate).getTime();
-  });
+  posts.sort((a, b) => b.pubDate.getTime() - a.pubDate.getTime());
 
   return rss({
-    title: `Jon's digital garden`,
+    title: `Jon Coleman`,
     description: "Notes and links about technical writing and web development",
     site: context.site,
-    items,
+    items: posts,
     customData: `<language>en-us</language>`,
   });
 }
